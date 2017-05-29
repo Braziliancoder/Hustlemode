495ab97df491d82a3e44506617f21f4b9853060a
Cassiel Hilbe <Cassiel@cassiels-macbook-pro-2.local>
2017-05-29 17:08:24 +0200

    Initial Commit

---
+++ Hustlemode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hustlemode
+//
+//  Created by Cassiel Hilbe on 29.05.17.
+//  Copyright © 2017 Cassiel Hilbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        init(contentsOf url: Sound/MoneyTrees.mp3) throws
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
